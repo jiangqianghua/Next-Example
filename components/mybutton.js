bf7f07efd15898d59e79c9ddcf5ada2e5b691c96
jiangqianghua <240437339@qq.com>
2020-07-19 21:31:14 +0800

    init

---
+++ components/mybutton.js
@@ -0,0 +1,10 @@
+export default (props) => {
+    return (
+        <div>
+            <button>{props.children}</button>
+    <p>{props.text}</p>
+        </div>
+       
+    
+    )
+}
---
+++ pages/home.js
@@ -0,0 +1,9 @@
+import MyButton from '../components/mybutton'
+function Home(){
+    return (
+       <div>
+           <MyButton text='这是text'>这是children内容</MyButton>
+       </div>
+    )
+}
+export default Home
